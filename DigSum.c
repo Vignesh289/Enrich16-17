80a45ef444d4464c348a2c7d4072bfb49075e5f2
Vignesh289 <vickyashokan17@gmail.com>
2016-12-26 23:46:05 +0530

    Create DigSum.c

---
+++ DigSum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,r,x=0,s=0;
+  clrscr();
+  printf("\nEnter the Number\t");
+  scanf("%d",&n);
+  while(n>0)
+  {
+    r=n%10;
+    s=s+r;
+    n=n/10;
+    x+=1;
+  }
+  printf("\nThe number of digits found is/are &d",x);
+  printf("\nThe sum of the digits are %d",s);
+  getch();
+}
